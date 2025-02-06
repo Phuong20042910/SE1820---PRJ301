b934dedee4de53e9d1dd43c57388707fb2af203b
Phuong20042910 <phamphuong2910204@gmail.com>
2025-02-06 11:43:32 +0700

    update

--- WebApplication_06/src/java/test/UserTest.java
+++ WebApplication_06/src/java/test/UserTest.java
@@ -15,12 +15,14 @@ import dto.UserDTO;
 public class UserTest {
     public static void main(String[] args) {
         UserDAO userDAO = new UserDAO();
-
-//      UserDTO udl = new UserDTO("TLN01", "Le Nhat Tung", "AD", "khong_co_password");
-//        userDAO.create(udl);
-       for(int i = 0; i < 10; i++){
+        UserDTO ud1 = new UserDTO("TLN01", "Le Nhat Tung", "AD", "khong_co_password");
+        userDAO.create(ud1);
+        for(int i = 0; i < 10; i++){
            UserDTO ud_i = new UserDTO("USER"+i, "Nguyen Van "+i, "US", "_"+i);
            userDAO.create(ud_i);
         }
+//        UserDTO ud2 = new UserDTO("TLN01", "Le Nhat Tung", "US", "Nothing");
+//        userDAO.update(ud2);
+            userDAO.delete("TLN01");
     }
 }    
